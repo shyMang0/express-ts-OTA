8a736010e4f830235597358ed875732c867af0ac
shyMang0 <carinodrex@gmail.com>
2024-01-22 17:33:22 +0800

    Create notes.model..ts

---
+++ src/db/models/notes.model..ts
@@ -0,0 +1,67 @@
+import { DataTypes, Model } from 'sequelize'
+import sequelize from '../config/sequelize'
+
+interface NotesAttributes {
+	id: string
+	note: string
+	created_at?: Date
+	updated_at?: Date
+}
+
+export interface NotesInput {
+	id: string
+	note: string
+	updated_at?: Date
+}
+
+export interface NotesOuput extends Required<NotesAttributes> {}
+
+class Notes extends Model<NotesAttributes, NotesInput> implements NotesAttributes {
+	public id!: string
+	public note!: string
+	public readonly created_at?: Date
+	public updated_at?: Date
+	public deleted_at?: Date
+}
+
+Notes.init(
+	{
+		id: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			primaryKey: true
+		},
+		note: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		created_at: {
+			type: DataTypes.DATE,
+			allowNull: true
+		},
+		updated_at: {
+			type: DataTypes.DATE,
+			allowNull: true
+		}
+	},
+	{
+		sequelize,
+		modelName: 'Notes',
+		timestamps: true,
+		paranoid: true,
+		createdAt: 'created_at',
+		updatedAt: 'updated_at',
+		deletedAt: 'deleted_at'
+	}
+)
+
+// Sync the model with the database (creates the table if it doesn't exist)
+Notes.sync({ force: false })
+	.then(() => {
+		console.log('User table created (if not exists)')
+	})
+	.catch((err) => {
+		console.error('Error creating User table:', err)
+	})
+
+export default Notes
