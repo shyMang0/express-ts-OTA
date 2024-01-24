73ff55a488ac92a5f617d4c95208f88a1b486658
shyMang0 <carinodrex@gmail.com>
2024-01-24 10:04:15 +0800

    after submit

--- src/api/validation/notes.validation.ts
+++ src/api/validation/notes.validation.ts
@@ -1,11 +1,23 @@
 import { NotesInput } from '@/db/models/notes.model'
 import { z, ZodError } from 'zod'
 import { formatZodError } from './zod.validation'
+import { fromZodError } from 'zod-validation-error'
 
 const CreateNoteSchema = z.object({
-	title: z.string().min(1, { message: 'Title is required and must be at least 1 character long' }).max(255),
-	body: z.string().min(1, { message: 'Body is required and must be at least 1 character long' })
+	title: z.string().min(1, { message: 'Title is required and must be at least 1 character long' }),
+	body: z.string().min(1, 'Body is required and must be at least 1 character long')
 })
+
+export const validateCreateNote = (data: any): NotesInput => {
+	try {
+		return <NotesInput>CreateNoteSchema.parse(data)
+	} catch (error) {
+		if (error instanceof ZodError) {
+			throw fromZodError(error)
+		}
+		throw error
+	}
+}
 
 const EditNoteSchema = z
 	.object({
@@ -15,17 +27,6 @@ const EditNoteSchema = z
 	.refine((data) => data.title !== undefined || data.body !== undefined, {
 		message: 'At least one of title or body should be present'
 	})
-
-export const validateCreateNote = (data: any): NotesInput => {
-	try {
-		return <NotesInput>CreateNoteSchema.parse(data)
-	} catch (error) {
-		if (error instanceof ZodError) {
-			throw formatZodError(error)
-		}
-		throw error
-	}
-}
 
 export const validateEditNote = (data: any): NotesInput => {
 	try {
